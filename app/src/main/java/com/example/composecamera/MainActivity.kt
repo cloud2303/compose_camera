d297f695726755c4bb38f56dab521453325a775d
cloud <2303058491@qq.com>
2022-11-06 18:28:10 +0800

    compose 请求权限

---
+++ app/src/main/java/com/example/composecamera/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.example.composecamera
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.example.composecamera.ui.theme.ComposeCameraTheme
+import com.google.accompanist.permissions.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCameraTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CameraScreen()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraScreen() {
+    val permissionsState = rememberMultiplePermissionsState(
+        permissions = listOf(android.Manifest.permission.CAMERA)
+    )
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(
+        key1 = lifecycleOwner,
+        effect = {
+            val observer = LifecycleEventObserver { _, event ->
+                if(event == Lifecycle.Event.ON_START) {
+                    permissionsState.launchMultiplePermissionRequest()
+                }
+            }
+            lifecycleOwner.lifecycle.addObserver(observer)
+
+            onDispose {
+                lifecycleOwner.lifecycle.removeObserver(observer)
+            }
+        }
+    )
+    permissionsState.permissions.forEach { permis ->
+        when(permis.permission)
+        {
+            android.Manifest.permission.CAMERA -> {
+                when {
+                    permis.status.isGranted ->
+                    {
+                        Text(text= "Camera Permission Granted")
+                    }
+                    permis.status.shouldShowRationale ->
+                    {
+                        Text(text= "Camera Permission  for taking Photo")
+                    }
+                    !permis.status.isGranted && !permis.status.shouldShowRationale ->
+                    {
+                        Text(text= "Camera Permission Denied.Go To App settings for enabling")
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeCameraTheme {
+        CameraScreen()
+    }
+}
