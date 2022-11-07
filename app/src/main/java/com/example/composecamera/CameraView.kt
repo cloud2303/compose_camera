88e2491d48ad76766bdef7c55537e37e8484bf6d
jiahui <2303058491@qq.com>
2022-11-07 09:59:35 +0800

    change camera executor

--- app/src/main/java/com/example/composecamera/CameraView.kt
+++ app/src/main/java/com/example/composecamera/CameraView.kt
@@ -2,71 +2,67 @@ package com.example.composecamera
 
 import android.content.ContentValues.TAG
 import android.util.Log
-import androidx.camera.core.*
-
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
 import java.nio.ByteBuffer
+import java.util.concurrent.Executors
+
 
 @Composable
 fun CameraView(
 ) {
     val lifecycleOwner = LocalLifecycleOwner.current
-    val context = LocalContext.current
-    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
 
     var state by remember { mutableStateOf("") }
+    val cameraExecutor = remember {
+        Log.d(TAG, "CameraView: 执行了x次")
+        Executors.newSingleThreadExecutor()
+    }
     Box(contentAlignment = Alignment.BottomCenter, modifier = Modifier.fillMaxSize()) {
-        AndroidView(factory = { ctx ->
-            val previewView = PreviewView(ctx)
-            val executor = ContextCompat.getMainExecutor(ctx)
-            cameraProviderFuture.addListener({
-                val cameraProvider = cameraProviderFuture.get()
-                val preview = Preview.Builder().build().also {
-                    it.setSurfaceProvider(previewView.surfaceProvider)
+        AndroidView(factory = {context->
+            val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+            val previewView = PreviewView(context)
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(previewView.surfaceProvider)
+            }
+            val camSelector =
+                CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                        Log.d(TAG, "Average luminosity: $luma")
+//                        state=luma.toString()
+                    })
                 }
 
-                val cameraSelector = CameraSelector.Builder()
-                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                    .build()
-
-                cameraProvider.unbindAll()
-
-                val imageAnalyzer = ImageAnalysis.Builder()
-                    .build()
-                    .also {
-                        it.setAnalyzer(executor, LuminosityAnalyzer { luma ->
-                            Log.d(TAG, "Average luminosity: $luma")
-                            state = luma.toString()
-                        })
-                    }
-
-                cameraProvider.bindToLifecycle(
+            try {
+                cameraProviderFuture.get().bindToLifecycle(
                     lifecycleOwner,
-                    cameraSelector,
+                    camSelector,
                     preview,
                     imageAnalyzer
                 )
-            }, executor)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
             previewView
-        }, modifier = Modifier.size(200.dp).clip(CircleShape).align(Alignment.Center))
+
+        }, modifier = Modifier.fillMaxSize()
+        )
         Button(onClick = {
 
         }) {
@@ -75,7 +71,7 @@ fun CameraView(
 
     }
 }
-typealias LumaListener = (luma: Double) -> Unit
+typealias LumaListener = (luma:  ByteArray?) -> Unit
 
 private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
 
@@ -88,13 +84,38 @@ private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnal
 
     override fun analyze(image: ImageProxy) {
 
-        val buffer = image.planes[0].buffer
-        val data = buffer.toByteArray()
-        val pixels = data.map { it.toInt() and 0xFF }
-        val luma = pixels.average()
-
-        listener(luma)
+//        val buffer = image.planes[0].buffer
+//        val data = buffer.toByteArray()
+//        val pixels = data.map { it.toInt() and 0xFF }
+//        val luma = pixels.average()
+        val nv21 = yuv420ToNv21(image)
+        listener(nv21)
 
         image.close()
+    }
+    private fun yuv420ToNv21(image: ImageProxy): ByteArray {
+        val planes = image.planes
+        val yBuffer = planes[0].buffer
+        val uBuffer = planes[1].buffer
+        val vBuffer = planes[2].buffer
+        val ySize = yBuffer.remaining()
+        val uSize = uBuffer.remaining()
+        val vSize = vBuffer.remaining()
+        val size = image.width * image.height
+        val nv21 = ByteArray(size * 3 / 2)
+        yBuffer[nv21, 0, ySize]
+        vBuffer[nv21, ySize, vSize]
+        val u = ByteArray(uSize)
+        uBuffer[u]
+
+        //每隔开一位替换V，达到VU交替
+        var pos = ySize + 1
+        for (i in 0 until uSize) {
+            if (i % 2 == 0) {
+                nv21[pos] = u[i]
+                pos += 2
+            }
+        }
+        return nv21
     }
 }
